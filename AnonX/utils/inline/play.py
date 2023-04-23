081900ce91ed8c412ce0513709d043744f6ab8c5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-23 11:59:33 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -63,12 +63,12 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/VeNom_HaiN_HuM"
+                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/BRANDRD_21"
             )
         ],
         [
             InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], url=f"https://t.me/FriendshipWorldGroup"
+                text=_["CLOSEMENU_BUTTON"], url=f"https://t.me/BRANDED_LINK"
             )
         ],
     ]
