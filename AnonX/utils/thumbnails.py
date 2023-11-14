6caad4c476027aa07d98b540603bf2bc788774d4
Datta xd <147732275+dattuxd@users.noreply.github.com>
2023-11-14 16:38:09 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -122,8 +122,8 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "BRANDED x MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "BRANDED MUSIC", fill="white", font = font2, stroke_width=2, stroke_fill="yellow", align ="left") 
+            image4.text((10, 10), "TOMXJERRY MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "TOM X MUSIC", fill="white", font = font2, stroke_width=2, stroke_fill="yellow", align ="left") 
 
             # title
             title1 = truncate(title)
