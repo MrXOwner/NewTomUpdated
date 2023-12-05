cbd57c8275e398181f6ba824d0ac6a0b67aaebb8
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-05 11:06:32 +0530

    Update group.py

--- AnonX/plugins/Robot/group.py
+++ AnonX/plugins/Robot/group.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from AnonXMusic import app
+from AnonX import app
 from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
